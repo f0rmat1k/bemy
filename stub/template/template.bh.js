a6f494fa80d3dcb412b2c32c645d9da300c6f6a4
Anton Grischenko <f0rmat1k@yandex-team.ru>
2015-04-30 22:39:35 +0500

    Fixed rename replacersfor new templates #25

--- stub/template/template.bh.js
+++ stub/template/template.bh.js
@@ -1,5 +1,5 @@
 module.exports = function (bh) {
     bh.match('{{blockName}}{{elemName}}{{modName}}{{modVal}}', function (ctx) {
-        {{cursor}}ctx.tag('div');
+        ctx.tag({{cursor}});
     });
 };
--- stub/template/template.priv.js
+++ stub/template/template.priv.js
@@ -2,7 +2,7 @@ module.exports = function (blocks) {
     blocks.declare('{{blockName}}{{elemName}}{{modName}}{{modVal}}', function (data) {
         return {
             block: '{{blockName}}',
-            content: ''{{cursor}}
+            content: '{{cursor}}'
         };
     });
 };
